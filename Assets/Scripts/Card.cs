7fc29b7e59c6bba9a7f6250cad6effedae28ef8e
1 <2@gmail.com>
2022-11-27 17:57:01 +0400

    Create UI scene, update card prefab

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,18 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+/// <summary>
+/// Карточка
+/// </summary>
 public class Card : MonoBehaviour
 {
+    /// <summary>
+    /// Картинка на карточке
+    /// </summary>
+    public RawImage CardRawImage;
+
+    /// <summary>
+    /// Обложка карточки
+    /// </summary>
+    public Image CoverImage;
+
+    /// <summary>
+    /// Спрайт лицевой стороны карточки
+    /// </summary>
+    Sprite FrontCardSprite;
+
+    /// <summary>
+    /// Спрайт обратной стороны карточки
+    /// </summary>
+    public Sprite BackCardSprite;
+
+    AnimFlipCard animFlipCard;
+
+
+    bool isLoadedImage;
+
+    /// <summary>
+    /// Признак загруженности картинки
+    /// </summary>
+    public bool IsLoadedImage
+    {
+        private set
+        {
+            isLoadedImage = value;
+        }
+
+        get
+        {
+            return isLoadedImage;
+        }
+    }
+
+    bool isOpenCard;
+    /// <summary>
+    /// Признак открытости картинки
+    /// </summary>
+    public bool IsOpenCard
+    {
+        set
+        {
+            isOpenCard = value;
+            animFlipCard.FlipCard(isOpenCard, () => 
+                {
+                    CoverImage.sprite = isOpenCard ? FrontCardSprite : BackCardSprite;
+                });            
+        }
+
+        get
+        {
+            return isOpenCard;
+        }
+    }
+
+    private void Awake()
+    {
+        FrontCardSprite = CoverImage.sprite;
+        animFlipCard = GetComponent<AnimFlipCard>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine(WebRequestHandler.GetImage(SetImage));
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetImage(Texture texture)
     {
-        
+        CardRawImage.texture = texture;
+        IsLoadedImage = true;
     }
+
+    
+    
 }
